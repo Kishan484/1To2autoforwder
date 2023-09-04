d13fa768b584fb80b24f4175f1f33082822f7a86
Kishan484 <121420677+Kishan484@users.noreply.github.com>
2023-09-04 20:55:21 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,11 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "Hi there! I am a channel auto forward bot. I can forward new messages & Media from one channel to another.\n\n• Source code:<b/> <b><a href='https://t.me/MOVIE_WORLD_IN_MOVIE'>Auto Forward Bot</a><b/>"             
+      START = "Hi there! I am a Private BOT ONLY ADMIN USE USING THIS BOT CHANNEL AUTO FORWARD BOT. \n I can forward new messages & Media from one channel to another. \n\n THANK YOU TOO 😎 @KISHAN484.\n AUTOMATIC FORWARD BOT.\n\n• Source code:<b/> <b><a href='https://t.me/MOVIE_WORLD_IN_MOVIE'>Auto Forward Bot</a><b/>"             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
+<b>🌏 INDIA TODAY:</b>KISHAN YADAV
+<b>🎬 MOVIE BOT:</b>@SOUTHMOVIEINBOT
 <b>👨‍💻 Developer:</b> @Kishan484
 <b>🌐Source Code:</b> kishan484"""
