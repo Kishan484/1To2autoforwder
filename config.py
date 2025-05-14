f0cbae6f683d0ce47ed1ef8f8657165b187d060a
Kishan484 <121420677+Kishan484@users.noreply.github.com>
2025-05-14 08:16:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
       API_HASH = getenv("API_HASH")
       API_ID = int(getenv("API_ID", 0))      
       BOT_TOKEN = getenv("BOT_TOKEN", "")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002497733939").replace("\n", " ").split(' '))
